ae68c19d5ac206005c57b61d10273437d575c059
CookieMonsta89 <jlstan06@hotmail.com>
2019-02-26 18:48:42 -0500

    mvp with comments to help understand what is happening which helps a little

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -46,19 +46,28 @@ def merge_sort_in_place(arr, l, r):
 def quick_sort( arr, low, high ):
     if high - low <= 0:
         return arr
-    
+        # base case above, returns array in last position minus zeroeth is less than 0
+        # first rule which is to have a base case
     startingPoint = low
-    for i in range(startingPoint+1, high-1):
-        if arr[i] < arr[startingPoint]:
+    # second rule which is have a path to the base case
+    for i in range(startingPoint+1, high+1):
+        # iterated less than zeroeth spot which now is 0 spot
+        if arr[i] < arr[startingPoint]: 
+            # setting variable to the iterated position
             mark = arr[i]
+            # setting iterated position to next position
             arr[i] = arr[startingPoint + 1]
+            # setting iterated position to mark which is value of iterated position of array
             arr[startingPoint + 1] = mark
-
+            # mark being set to arrow low which is zeroeth 
             mark = arr[startingPoint]
+            # zeroeth being set to next position 
             arr[startingPoint] = arr[startingPoint + 1]
+            # setting next position equal to mark which is the arr[low]
             arr[startingPoint + 1] = mark
+            # incrementing starting point and setting it equal to one.
             startingPoint += 1
-
+    # this is the third rule here which is calling the function 
     arr = quick_sort(arr, low, startingPoint - 1)
     arr = quick_sort(arr, startingPoint + 1, high)
 
